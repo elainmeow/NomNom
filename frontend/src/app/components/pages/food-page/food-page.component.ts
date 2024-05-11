3c88ef8dd9039c5d7f7353845ddeef8e7f36d08a
Elain <nhulmq21411ca@st.uel.edu.com>
2024-05-11 11:40:16 +0700

    NomNom

---
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FoodService } from '../../../services/food.service';
+import { Food } from '../../../shared/models/Food';
+import { CartService } from '../../../services/cart.service';
+
+@Component({
+  selector: 'app-food-page',
+  templateUrl: './food-page.component.html',
+  styleUrl: './food-page.component.css'
+})
+export class FoodPageComponent implements OnInit {
+  food!: Food;
+  constructor(activatedRoute:ActivatedRoute, foodService:FoodService,
+    private cartService:CartService, private router: Router) {
+    activatedRoute.params.subscribe((params) => {
+      if(params.id)
+        foodService.getFoodById(params.id).subscribe(serverFood => {
+          this.food = serverFood;
+      });
+    })
+
+   }
+
+  ngOnInit(): void {
+  }
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+
+  }
+}
